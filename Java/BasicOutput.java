d358c2262dec250881c51cd65f8d78048f4217bf
Nghia Vo Nhu Duc <nghiathechosenone@gmail.com>
2021-10-16 13:32:39 +0700

    Create BasicOutput.java

---
+++ Java/BasicOutput.java
@@ -0,0 +1,16 @@
+public class BasicOutput
+{ 
+    public static void main(String args[])
+    {
+        int i = 21;
+        char ch = 'I';
+        double d = 2.1;
+        boolean b = true;
+        String s = "Irene Adler";
+        System.out.println(i);
+        System.out.println(ch);
+        System.out.println(d);
+        System.out.println(b);
+        System.out.println(s);
+    }
+}

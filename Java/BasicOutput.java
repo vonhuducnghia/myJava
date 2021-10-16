38cdc14b0d66f3168797508d88516fdb0ad20811
Nghia Vo Nhu Duc <nghiathechosenone@gmail.com>
2021-10-16 15:52:22 +0700

    Update BasicOutput.java

--- Java/BasicOutput.java
+++ Java/BasicOutput.java
@@ -3,14 +3,15 @@ public class BasicOutput
     static int i = 21;
     static char ch = 'I';
     static double d = 2.1;
+    static float f = 20.01f;
     static boolean b = true;
     static String s = "Irene Adler";
     public static void main(String args[])
     {
-        
         System.out.println(i);
         System.out.println(ch);
         System.out.println(d);
+        System.out.println(f);
         System.out.println(b);
         System.out.println(s);
     }

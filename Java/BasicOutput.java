2dca0fe640b5756dffeacd76ebcba2f479b98f8c
Nghia Vo Nhu Duc <nghiathechosenone@gmail.com>
2021-10-23 14:06:22 +0700

    Update BasicOutput.java

--- Java/BasicOutput.java
+++ Java/BasicOutput.java
@@ -32,10 +32,26 @@
         System.out.println(num1 + "+" + num2 + "= " + num1 + num2);
     }
 }*/
-public class BasicOutput
+/*public class BasicOutput
 {
     public static void main(String args[])
     {
         System.out.println("\"Hello the World!\""); // Using Escape Sequence
+    }
+}*/
+
+public class BasicOutput
+{
+    public static void main(String[] args)
+    {
+        byte b;
+        int i = 257;
+        double d = 323.142;
+        System.out.println("\nConversion of int to byte."); b = (byte) i;
+        System.out.println("i and b " + i + " " + b);
+        System.out.println("\nConversion of double to int."); i = (int) d;
+        System.out.println("d and i " + d + " " + i);
+        System.out.println("\nConversion of double to byte."); b = (byte) d;
+        System.out.println("d and b " + d + " " + b);
     }
 }

eeeadf19d49ef831cd6e97eb3897e9f64949ddc7
Nghia Vo Nhu Duc <nghiathechosenone@gmail.com>
2021-10-30 14:38:35 +0700

    Update BasicOutput.java

--- Java/BasicOutput.java
+++ Java/BasicOutput.java
@@ -143,7 +143,7 @@ public class BasicOutput
 // Quadratic solving algorithm
 import java.util.Scanner;  
 public class BasicOutput
-    {
+{
     public static void main(String[] Strings)
     {
         Scanner input = new Scanner(System.in);
@@ -153,21 +153,31 @@ public class BasicOutput
         double b = input.nextDouble();
         System.out.print("Enter the value of c: ");
         double c = input.nextDouble();
-        double d = b * b - 4.0 * a * c;
-        if (d> 0.0)
-    {
-        double r1 = (-b + Math.pow(d, 0.5)) / (2.0 * a);
-        double r2 = (-b - Math.pow(d, 0.5)) / (2.0 * a);
-        System.out.println("The roots are " + r1 + " and " + r2);
-    }
-    else if (d == 0.0)
-    {
-        double r1 = -b / (2.0 * a);
-        System.out.println("The root is " + r1);
-    }
-    else
-    {
-        System.out.println("Roots are not real.");
-    }
+        if (a == 0)
+        {
+            double x = 0;
+            x = -b/c;
+            System.out.println("x = " + x);
+        }
+        else
+        {
+            double d = 0;
+            d = b * b - 4.0 * a * c;
+            if (d > 0.0)
+                {
+                    double r1 = (-b + Math.pow(d, 0.5)) / (2.0 * a);
+                    double r2 = (-b - Math.pow(d, 0.5)) / (2.0 * a);
+                    System.out.println("The roots are " + r1 + " and " + r2);
+                }
+                else if (d == 0.0)
+                {
+                    double r1 = -b / (2.0 * a);
+                    System.out.println("The root is " + r1);
+                }
+                else
+                {
+                    System.out.println("Roots are not real.");
+                }
+        }
     }
 }

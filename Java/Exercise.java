6b5e44d2c94c61fcb7ea7c6aea8be26053ddad1b
Nghia Vo Nhu Duc <nghiathechosenone@gmail.com>
2021-10-30 15:33:42 +0700

    Create Exercise.java

---
+++ Java/Exercise.java
@@ -0,0 +1,33 @@
+// Exercise 3
+/*import java.util.Scanner;
+public class Exercise
+{
+    public static void main(String[] args)
+    {
+        int year;
+        System.out.println("Enter an Year :: ");
+        Scanner sc = new Scanner(System.in);
+        year = sc.nextInt();
+        if (((year % 4 == 0) && (year % 100!= 0)) || (year%400 == 0))
+           System.out.println("Specified year is a leap year");
+        else
+           System.out.println("Specified year is not a leap year");
+     }
+}*/
+
+//Exercise 5
+import java.util.Scanner;
+public class Exercise
+{
+    public static void main(String[] args)
+    {
+        int day;
+        int month;
+        int year;
+        Scanner sc = new Scanner(System.in);
+        day = sc.nextInt();
+        month = sc.nextInt();
+        year = sc.nextInt();
+        
+    }
+}

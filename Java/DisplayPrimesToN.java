beb68de6d52b9a88606e767acf6e8dab5920dbf7
Nghia Vo Nhu Duc <nghiathechosenone@gmail.com>
2021-10-30 18:16:23 +0700

    Update DisplayPrimesToN.java

--- Java/DisplayPrimesToN.java
+++ Java/DisplayPrimesToN.java
@@ -8,7 +8,7 @@ class PrimeNumbers2
       int num = 0;
       //Empty String
       String primeNumbers = "";
-      System.out.println("Enter the value of n:");
+      System.out.print("Enter the value of n: ");
       int n = scanner.nextInt();
       scanner.close();
       for (i = 1; i <= n; i++)  	   
@@ -16,17 +16,17 @@ class PrimeNumbers2
          int counter = 0; 		  
          for(num = i; num >= 1; num--)
          {
-	    if(i%num == 0)
-	    {
-		counter = counter + 1;
-	    }
-	 }
-	 if (counter == 2)
-	 {
-	    //Appended the Prime number to the String
-	    primeNumbers = primeNumbers + i + " ";
-	 }	
-      }	
+	         if(i%num == 0)
+	      {
+		      counter = counter + 1;
+	      }
+	   }
+	      if (counter == 2)
+	      {
+	         //Appended the Prime number to the String
+	         primeNumbers = primeNumbers + i + " ";
+	      }
+      }
       System.out.println("Prime numbers from 1 to n are :");
       System.out.println(primeNumbers);
    }

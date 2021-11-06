26bee9b7b224e052c8f3734959f28d71e0f6633f
Nghia Vo Nhu Duc <nghiathechosenone@gmail.com>
2021-11-06 14:48:11 +0700

    update

--- Java/Factorial.java
+++ Java/Factorial.java
@@ -5,10 +5,10 @@ public class Factorial
     {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a Factorial: ");
-        int n = scanner.nextInt();
+        double n = scanner.nextDouble();
         if (n == 0)
             System.out.println("1");
-        int factorial = n;
+        double factorial = n;
         scanner.close();
         if (n >= 1)
         {
@@ -20,17 +20,3 @@ public class Factorial
         }
     }
 }
-
-/*class Factorial
-{
-    public static void main(String args[])
-    {
-        int i,fact=1;
-        int number=11;//It is the number to calculate factorial
-        for(i=1;i<=number;i++)
-        {
-            fact=fact*i;
-        }
-        System.out.println("Factorial of "+number+" is: "+fact);
-    }
-}*/

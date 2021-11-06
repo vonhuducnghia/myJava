9f703ef2926f4629d4e398bf9da348d1595789b2
Nghia Vo Nhu Duc <nghiathechosenone@gmail.com>
2021-11-06 14:45:40 +0700

    update

--- Java/Factorial.java
+++ Java/Factorial.java
@@ -20,3 +20,17 @@ public class Factorial
         }
     }
 }
+
+/*class Factorial
+{
+    public static void main(String args[])
+    {
+        int i,fact=1;
+        int number=11;//It is the number to calculate factorial
+        for(i=1;i<=number;i++)
+        {
+            fact=fact*i;
+        }
+        System.out.println("Factorial of "+number+" is: "+fact);
+    }
+}*/
